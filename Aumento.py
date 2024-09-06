cb19444683db3709db5e2e6573f003d8224059c8
Guilherme Leite <guiroleite@gmail.com>
2024-09-05 22:49:30 -0300

    toma mai coisa

---
+++ Aumento.py
@@ -0,0 +1,7 @@
+def aumentoSalario():
+    Salario1 = 1000
+    SalarioAumento = Salario1 * 0.15
+    SalarioAumentado = Salario1 + SalarioAumento
+    print(f"Seu salario é {SalarioAumentado}")
+
+aumentoSalario()
---
+++ excuta.py
@@ -0,0 +1,9 @@
+
+def executa():
+    a = 10
+    b = 5
+    a = b
+    b = b + a
+    print(a,b) 
+
+executa()
